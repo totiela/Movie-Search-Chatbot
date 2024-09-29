b98c386f02d706f9e0625a5e40ade26ed25cb437
totiela <156684893+totiela@users.noreply.github.com>
2024-09-29 20:51:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ from download_db import download_and_prepare
 import langchain
 # import os
 
-langchain_tracing_v2 = st.secrets["LANGCHAIN_TRACING_V2"]
-langchain_endpoint = st.secrets["LANGCHAIN_ENDPOINT"]
-langchain_api_key = st.secrets["LANGCHAIN_API_KEY"]
-langchain_project = st.secrets["LANGCHAIN_PROJECT"]
+# langchain_tracing_v2 = st.secrets["LANGCHAIN_TRACING_V2"]
+# langchain_endpoint = st.secrets["LANGCHAIN_ENDPOINT"]
+# langchain_api_key = st.secrets["LANGCHAIN_API_KEY"]
+# langchain_project = st.secrets["LANGCHAIN_PROJECT"]
+
+LANGCHAIN_TRACING_V2=true
+LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
+LANGCHAIN_API_KEY="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"
+LANGCHAIN_PROJECT="pr-untimely-hand-46"
 
 # os.environ["LANGCHAIN_TRACING_V2"] = str(langchain_tracing_v2) # Преобразуем boolean в строку
 # os.environ["LANGCHAIN_ENDPOINT"] = langchain_endpoint
